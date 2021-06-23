173b3e05aec58b5a9567cd2a580150d820413561
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-06-23 11:13:19 -0700

    laid out html structure of create page form

--- src/CreatePage.js
+++ src/CreatePage.js
@@ -4,7 +4,31 @@ export default class CreatePage extends Component {
     render() {
         return (
             <div>
-                <p>Create an Owl!</p>
+                <form>
+                    <label>
+                        Owl Name
+                        <input type="text"></input>
+                    </label>
+                    <label>
+                        Note / Description
+                        <input type="text"></input>
+                    </label>
+                    <label>
+                        Habitat
+                        <input type="text"></input>
+                    </label>
+                    <label>
+                        Price
+                        <input type="text"></input>
+                    </label>
+                        Endangered Status:
+                        <select>
+                            <option value="Critical">Critical</option>
+                            <option value="Moderate">Moderate</option>
+                            <option value="None">None</option>
+                        </select>
+                    <button type="submit">Create!</button>
+                </form>
             </div>
         )
     }

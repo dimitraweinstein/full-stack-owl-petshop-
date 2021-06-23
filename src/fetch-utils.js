814bc9a11b47a2f9a46239e5244bc143d686ae44
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-06-23 11:00:22 -0700

    wrote all fetch utils functions

--- src/fetch-utils.js
+++ src/fetch-utils.js
@@ -1,27 +1,43 @@
-// import request from 'superagent';
+import request from 'superagent';
 
-// const URL = 'https://vast-springs-03582.herokuapp.com/';
+const URL = 'https://vast-springs-03582.herokuapp.com';
 
-// export async function getAllOwls() {
+export async function getAllOwls() {
+    const data = await request.get(`${URL}/owls/`);
 
-// }
+    return data.body;
+}
 
-// export async function getOneOwl() {
-    
-// }
+export async function getOneOwl(id) {
+    const data = await request.get(`${URL}/owls/${id}`);
 
-// export async function updateOwl() {
-    
-// }
+    return data.body;
+}
 
-// export async function createOwl() {
-    
-// }
+export async function updateOwl(owlData, id) {
+    const data = await request
+        .put(`${URL}/owls/${id}`)
+        .send(owlData);
 
-// export async function getAllEndangered() {
-    
-// }
+    return data.body;
+}
 
-// export async function deleteOwl() {
-    
-// }
+export async function createOwl(owlData) {
+    const data = await request
+        .post(`${URL}/owls/`)
+        .send(owlData);
+
+    return data.body;
+}
+
+export async function getAllEndangered() {
+    const data = await request.get(`${URL}/endangered`);
+
+    return data.body;
+}
+
+export async function deleteOwl(id) {
+    const data = await request.get(`${URL}/owls/${id}`);
+
+    return data.body;
+}

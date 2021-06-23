47ea64d884b3ab2bfdf1a751ba136f761e632345
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-06-23 12:35:24 -0700

    list page rendering correctly

--- src/ListPage.js
+++ src/ListPage.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react'
-// import { getAllOwls } from './fetch-utils.js';
-// import { Link } from 'react-router-dom';
+import { getAllOwls } from './fetch-utils.js';
+import { Link } from 'react-router-dom';
 
 export default class ListPage extends Component {
     state = {
         owls: []
     }
 
-    // componentDidMount = async () => {
-    //     const owls = await getAllOwls();
+    componentDidMount = async () => {
+        const owls = await getAllOwls();
 
-    //     this.setState({ owls: owls })
-    // }
+        this.setState({ owls: owls })
+    }
 
     render() {
         return (
-            <div>
-                <p>List Page!</p>
+            <div className="owls">
+                <p>See Our Owl Inventory!</p>
+                {
+                    this.state.owls.map(owl => <Link to={`/owls/${owl.id}`}>
+                        <div>
+                            <p>{owl.name}</p>
+                            <p>{owl.note}</p>
+                            <p>{owl.habitat}</p>
+                            <p>{owl.price}</p>
+                            <p>{ owl.endangered }</p>
+                        </div>
+                    </Link>)
+                    
+                }
             </div>
         )
     }
